89bdc41ca2b3199e1b3ebdb1473653b54db47328
Mushfiqur Rahman Siddiqui <mushfiq.rahat39@gmail.com>
2025-12-30 16:31:19 +0600

    done

--- polymorphism.py
+++ polymorphism.py
@@ -1,13 +1,18 @@
 
-class Vehicle:
-
+class Vehicles:
     def __init__(self, brand, model, year):
         self.brand = brand
         self.model = model
         self.year = year
 
+    def start(self):
+        print("Vehicle is starting")
 
-class Car(Vehicle):
+    def stop(self):
+        print("Vehicle is stopping")
+
+
+class Car(Vehicles):
     def  __init__(self, brand, model, year, no_of_doors):
         super().__init__(brand, model, year)
         self.no_of_doors = no_of_doors
@@ -16,34 +21,45 @@ class Car(Vehicle):
         print("Car is starting")
 
     def stop(self):
-        print("Car is stopping")
+       print("Car is stopping")
 
-class Bike(Vehicle):
+class Bike(Vehicles):
     def __init__(self, brand, model, year, no_of_wheels):
         super().__init__(brand, model, year)
         self.no_of_wheels = no_of_wheels
 
     def start(self):
-        print("Bike is starting")
+       print("Bike is starting")
 
     def stop(self):
-        print("Bike is stopping")
+       print("Bike is stopping")
 
-vehicles = [
+class Airplane(Vehicles):
+    def __init__(self, brand, model, year, no_of_doors):
+        super().__init__(brand, model, year)
+        self.no_of_wheels = no_of_doors
+
+    def start(self):
+       print("Airplane is starting")
+
+    def stop(self):
+       print("Airplane is stopping")
+
+vehicles:  list[Vehicles] = [
     Car("Ford", "GT", 2008, 5),
-    Bike("Honda", "Scoopy", 2009, 2)
+    Bike("Honda", "Scoopy", 2009, 2),
+    Airplane("Boeing", "787", 2011, 16)
 ]
-
 for vehicle in vehicles:
-    if isinstance(vehicle, Car):
-        print(f"Inspecting {vehicle.brand} {vehicle.model} ({type(vehicle).__name__})")
-        vehicle.start()
-        vehicle.stop()
+    print(f"Inspecting {vehicle.brand} {vehicle.model} {type(vehicle).__name__}")
+    vehicle.start()
+    vehicle.stop()
 
-    elif isinstance(vehicle, Bike):
-        print(f"Inspecting {vehicle.brand} {vehicle.model} ({type(vehicle).__name__})")
-        vehicle.start()
-        vehicle.stop()
 
-    else:
-        raise Exception("Object is not a valid vehicle")
+
+
+
+
+
+
+
